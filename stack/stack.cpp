ea4a65b29ab70d5eeff92f779c1466aacb1e9612
Daniel Bergey <bergey@simspace.com>
2020-12-14 13:33:54 +0000

    C++ template to make Stack generic in element type

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -3,49 +3,44 @@
 using namespace std;
 
 // This very simple stack has a fixed size (does not grow)
-// and can only store int
 
-class Stack {
-    int **head;
+template<class A> class Stack {
+    A **head;
     size_t top;
     size_t max_size;
 
 public:
     class Overflow { };
-    Stack(size_t max_size = 10);
-    int* pop();
-    void push(int* a);
+
+    Stack(size_t requested_size = 10) {
+        max_size = requested_size;
+        top = 0;
+        head = new A*[max_size];
+    }
+
+    A* pop() {
+        if (top == 0) {
+            return 0;
+        } else {
+            top -= 1;
+            return head[top];
+        }
+    }
+
+    void push(A* a) {
+        if (top == max_size) {
+            throw Overflow(); 
+        } else {
+            head[top] = a;
+            top += 1;
+        }
+    }
 };
 
-Stack::Stack(size_t requested_size) {
-    max_size = requested_size;
-    top = 0;
-    head = new int*[max_size];
-}
 
-int* Stack::pop() {
-    if (top == 0) {
-        return 0;
-    } else {
-        top -= 1;
-        return head[top];
-    }
-}
-
-void Stack::push(int* a) {
-    if (top == max_size) {
-        throw Overflow(); 
-    } else {
-        head[top] = a;
-        top += 1;
-    }
-       
-    
-    
-}
 
 int main() {
-    Stack s = Stack();
+    Stack<int> s = Stack<int>();
     int* a = new int;
     *a = 1;
     s.push(a);
